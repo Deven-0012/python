ec1557cbecaabf4c8046191a9fadb68b85e4bf6f
Deven <devendesai007@gmail.com>
2023-02-24 19:48:16 +0530

    "131st video"

---
+++ udemy142.py
@@ -0,0 +1,14 @@
+l = [1,2,3,4,5,1,2,3,4,5]
+print(l.count(3))
+l.append(5)
+print(l.count(5))
+l.insert(2,3)
+print(l)
+
+x = [1,2,3]
+x.append([4,5])
+print(x)
+
+z = [1,2,3,]
+z.extend([4,5])
+print(z)
